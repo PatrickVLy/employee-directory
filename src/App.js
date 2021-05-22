ef0661c38152db241f61bb16ccaef8ccfd5cffc7
Patrick Ly <patrickly72@gmail.com>
2021-05-21 22:46:14 -0700

    added app function

--- src/App.js
+++ src/App.js
@@ -3,3 +3,13 @@ import FilterInput from "./FilterInput";
 import Table from "./Table";
 import { getUsers } from "./API";
 import "./App.css";
+
+
+function App() {
+    const [initialUsers, updateAvailableUsers] = useState([]);
+    const [usersToRender, updateUsersToRender] = useState([]);
+  
+    useEffect(() => {
+      getUsers().then(({ data: { results } }) => updateAvailableUsers(results));
+    }, []);
+  
