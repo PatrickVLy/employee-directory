a96bb8ef07e7279bd1e90055ad5a2e3cf3642e0e
Patrick Ly <patrickly72@gmail.com>
2021-05-21 22:50:13 -0700

    added useeffect and usestate hopks to filter

--- src/FilterInput.js
+++ src/FilterInput.js
@@ -1,1 +1,17 @@
 import React, { useEffect, useState } from "react";
+
+const FilterInput = ({ users, updateUsers }) => {
+    const [inputValue, updateInput] = useState("");
+  
+    useEffect(() => {
+      const filteredUsers =
+        inputValue === ""
+          ? users
+          : users.filter(
+              ({ name: { first } }) =>
+                first.toLowerCase().indexOf(inputValue.toLowerCase()) >= 0
+            );
+  
+      updateUsers(filteredUsers);
+    }, [inputValue, users]);
+  
